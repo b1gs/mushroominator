b3bfb2650e3a693fd0370fb762efb341d3d36f2d
b1gs <b1gsmen@gmail.com>
2023-07-11 18:10:29 +0300

    initial commit

---
+++ src/main/java/com/b1gs/controllers/ControllersApplication.java
@@ -0,0 +1,13 @@
+package com.b1gs.controllers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControllersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControllersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/b1gs/controllers/ControllersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.b1gs.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControllersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
