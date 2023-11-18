7668b6f97d08cac31636703b90d0861df4180865
b1gs <b1gsmen@gmail.com>
2023-11-18 14:03:30 +0200

    Add codeDeploy configuration

---
+++ scripts/after-install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+
+echo "inside a script!!!!"
+
+sudo chown -R ubuntu:ubuntu /var/log/mushroominator/*
+
+sudo chown -R ubuntu:ubuntu /home/ubuntu/projects/tmp/mushroominator/*
+
+rm -rf ~/projects/mushroominator
+#
+
+## Unzip the deployment package
+sudo mkdir ~/projects/mushroominator
+
+sudo chown -R ubuntu:ubuntu ~/projects/mushroominator
+
+sudo rm -rf /home/ubuntu/projects/tmp/mushroominator/scripts
+
+sudo rm -rf /home/ubuntu/projects/tmp/mushroominator/appspec.yml
+
+sudo cp -r /home/ubuntu/projects/tmp/mushroominator/* ~/projects/mushroominator
+
+sudo rm -rf /home/ubuntu/projects/tmp/mushroominator
+
+sudo chown -R ubuntu:ubuntu ~/projects/mushroominator/mushroominator-0.0.1-SNAPSHOT.jar
+
+cd ~/projects/mushroominator
+
+ls -al
+
+echo "Starting java application..."
+
+java -jar mushroominator-0.0.1-SNAPSHOT.jar > /var/log/mushroominator/output.log 2>&1
