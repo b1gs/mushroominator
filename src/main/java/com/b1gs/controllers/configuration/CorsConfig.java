16260c8995e73bdfd46805a1c3f9e0e420c43340
b1gs <b1gsmen@gmail.com>
2023-11-20 23:32:19 +0200

    Adding cors configuration

--- src/main/java/com/b1gs/controllers/configuration/CorsConfig.java
+++ src/main/java/com/b1gs/controllers/configuration/CorsConfig.java
@@ -1,16 +1,21 @@
 package com.b1gs.controllers.configuration;
 
+import com.b1gs.controllers.configuration.properties.CorsProperties;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class CorsConfig implements WebMvcConfigurer {
+
+    private final CorsProperties corsProperties;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200", "http://192.168.0.204:4200")
+                .allowedOrigins(corsProperties.getOrigins().toArray(new String[0]))
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
     }
---
+++ src/main/java/com/b1gs/controllers/configuration/properties/CorsProperties.java
@@ -0,0 +1,17 @@
+package com.b1gs.controllers.configuration.properties;
+
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Data
+@Component
+@ConfigurationProperties(prefix = "cors")
+public class CorsProperties {
+
+   private List<String> origins;
+
+}
